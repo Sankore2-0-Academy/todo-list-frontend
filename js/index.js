e019950b1ff7793a33e85400bc8fa10c8f5c602f
McOtech <otienopaul36@gmail.com>
2022-05-19 17:59:38 +0300

    Todo-list Frontend

---
+++ js/index.js
@@ -0,0 +1,142 @@
+if (window.location.pathname === '/auth.html') {
+  // Check if wallet is linked
+  if (isLoggedIn()) {
+    window.location.href = '/index.html';
+  } else {
+    // Access login button DOM element
+    const loginBtn = document.querySelector('#login-btn');
+    loginBtn.onclick = (event) => {
+      login();
+    };
+  }
+} else if (window.location.pathname === '/index.html') {
+  if (!isLoggedIn()) {
+    window.location.href = '/auth.html';
+  }
+  document.querySelector('#app-name').textContent = APP_NAME;
+  document.querySelector('#wallet-id').textContent = getAccount();
+  const newTaskForm = document.querySelector('#new-task');
+  const logoutBtn = document.querySelector('#logout');
+
+  /**
+   * =====================================================
+   * Smart Contract Calls
+   * =====================================================
+   */
+
+  // Logout User
+  logoutBtn.onclick = () => {
+    logout();
+    window.location.reload();
+  };
+
+  // Adds submitted task
+  newTaskForm.onsubmit = async (event) => {
+    event.preventDefault();
+    const taskTitle = event.target.newTaskTitle.value;
+    const createdTask = await newTask(taskTitle);
+    const pendingTaskView = document.querySelector('#pending-tasks');
+    pendingTaskView.innerHTML += `<li
+                        class="list-group-item d-flex align-items-center justify-content-between border-0 mb-2 rounded"
+                        style="background-color: #f4f6f7"
+                      >
+                        <div class="d-flex align-items-center">
+                          ${createdTask.title}
+                        </div>
+                        <button onclick="eventWrap(${createdTask.id}, ${startTask})" class="btn btn-sm btn-secondary m-2 text-light">
+                          Start
+                        </button>
+                      </li>`;
+    newTaskForm.reset();
+  };
+
+  // Displays tasks
+  displayTasks();
+}
+
+async function displayTasks() {
+  // Retreive all tasks from smart contract
+  const allTasks = await getAllTasks();
+
+  let pending = '';
+  let active = '';
+  let complete = '';
+
+  // Loop through the list of tasks if any and wrap them in relevant HTML elemnts
+  for (let i = 0; i < allTasks.length; i++) {
+    const task = allTasks[i];
+    if (task.status === 0) {
+      pending += taskElement(i, task.status, task.title);
+    } else if (task.status === 1) {
+      active += taskElement(i, task.status, task.title);
+    } else {
+      complete += taskElement(i, task.status, task.title);
+    }
+  }
+
+  // Reflect the change in DOM
+  updateTaskViews(pending, active, complete);
+}
+
+function taskElement(id, status, title) {
+  switch (status) {
+    case 0:
+      return `<li
+                        class="list-group-item d-flex align-items-center justify-content-between border-0 mb-2 rounded"
+                        style="background-color: #f4f6f7"
+                      >
+                        <div class="d-flex align-items-center">
+                          ${title}
+                        </div>
+                        <button onclick="eventWrap(${id}, ${startTask})" class="btn btn-sm btn-secondary m-2 text-light">
+                          Start
+                        </button>
+                      </li>`;
+    case 1:
+      return `<li
+                        class="list-group-item d-flex align-items-center justify-content-between border-0 mb-2 rounded"
+                        style="background-color: #f4f6f7"
+                      >
+                        <div class="d-flex align-items-center">
+                          ${title}
+                        </div>
+                        <button onclick="eventWrap(${id}, ${completeTask})" class="btn btn-sm btn-secondary m-2 text-light">
+                          Complete
+                        </button>
+                      </li>`;
+    case 2:
+      return `<li
+                        class="list-group-item d-flex align-items-center justify-content-between border-0 mb-2 rounded"
+                        style="background-color: #f4f6f7"
+                      >
+                        <div class="d-flex align-items-center">
+                          ${title}
+                        </div>
+                        <button onclick="eventWrap(${id}, ${removeTask})" class="btn btn-sm btn-danger m-2 text-light">
+                          Delete
+                        </button>
+                      </li>`;
+
+    default:
+      '';
+  }
+}
+
+function updateTaskViews(pending, active, completed) {
+  const pendingTaskView = document.querySelector('#pending-tasks');
+  const activeTaskView = document.querySelector('#active-tasks');
+  const completedTaskView = document.querySelector('#completed-tasks');
+
+  pendingTaskView.innerHTML = pending;
+  activeTaskView.innerHTML = active;
+  completedTaskView.innerHTML = completed;
+}
+
+async function eventWrap(id, callback) {
+  const res = await callback(id);
+  if (res) {
+    window.location.reload();
+  } else {
+    alert(`Operation as not successful!`);
+  }
+}
---
+++ js/utils.js
@@ -0,0 +1,120 @@
+const nearConfig = getConfig('development');
+
+// Initialize contract & set global variables
+(async function () {
+  // Initialize connection to NEAR testnet
+  window.near = await nearApi.connect(nearConfig);
+
+  // Initializing Wallet based Account. It can work with NEAR testnet wallet that
+  // is hosted at https://wallet.testnet.near.org
+  window.walletConnection = new nearApi.WalletConnection(near);
+  window.account = await walletConnection.account();
+
+  // Initializing our contract APIs by contract name and configuration
+  window.contract = new nearApi.Contract(
+    account, // the account object that is connecting
+    CONTRACT_NAME,
+    {
+      // name of contract you're connecting to
+      viewMethods: [], // view methods do not change state but usually return a value
+      changeMethods: [
+        'newTask',
+        'showTask',
+        'showAllTasks',
+        'startTask',
+        'completeTask',
+        'removeTask',
+      ], // change methods modify state
+      sender: account, // account object to initialize and sign transactions.
+    }
+  );
+})(window);
+
+const isLoggedIn = () => {
+  return window.walletConnection.isSignedIn();
+};
+
+const getAccount = () => {
+  return window.walletConnection.getAccountId();
+};
+
+const login = () => {
+  // Allow the current app to make calls to the specified contract on the
+  // user's behalf.
+  // This works by creating a new access key for the user's account and storing
+  // the private key in localStorage.
+  if (!isLoggedIn()) {
+    window.walletConnection.requestSignIn(
+      CONTRACT_NAME, // contract requesting access
+      APP_NAME // optional
+    );
+  } else {
+    alert(`Already logged in as ${getAccount()}`);
+  }
+};
+
+const logout = () => {
+  if (isLoggedIn()) {
+    window.walletConnection.signOut();
+    // Page redirect
+    window.location.reload();
+  } else {
+    alert(`Already logged Out!`);
+  }
+};
+
+/**
+ * ====================================================================
+ * Smart Contract Menthods
+ * ====================================================================
+ */
+
+const newTask = async (title) => {
+  if (isLoggedIn()) {
+    const response = await contract.newTask({
+      title,
+    });
+    return response;
+  }
+  return null;
+};
+
+const getTask = async (taskId) => {
+  if (isLoggedIn()) {
+    const response = await contract.showTask({ taskId });
+    return response;
+  }
+  return null;
+};
+
+const getAllTasks = async () => {
+  if (isLoggedIn()) {
+    const response = await contract.showAllTasks();
+    return response;
+  }
+  return [];
+};
+
+const startTask = async (taskId) => {
+  if (isLoggedIn()) {
+    const response = await contract.startTask({ taskId });
+    return response;
+  }
+  return false;
+};
+
+const completeTask = async (taskId) => {
+  if (isLoggedIn()) {
+    const response = await contract.completeTask({ taskId });
+    return response;
+  }
+  return false;
+};
+
+const removeTask = async (taskId) => {
+  if (isLoggedIn()) {
+    const response = await contract.removeTask({ taskId });
+    return response;
+  }
+  return null;
+};
